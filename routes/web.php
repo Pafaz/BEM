a9041b1e514a8bf9b585cbbfe33d84d3ad282679
Osas997 <larasatyt83@gmail.com>
2024-06-03 13:22:19 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    
 });
+
 Route::get('/kastrat', [KastratController::class, 'index'])->name('kastrat.index');
-Route::get('/kastrat', [KastratController::class, 'edit'])->name('kastrat.edit');
+Route::get('/kastrat/{id}', [KastratController::class, 'edit'])->name('kastrat.edit');
 Route::patch('/kastrat', [KastratController::class, 'update'])->name('kastrat.update');
 Route::delete('/kastrat', [KastratController::class, 'destroy'])->name('kastrat.destroy');
 
 Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::get('/admin', [AdminController::class, 'edit'])->name('admin.edit');
+Route::get('/admin/{id}', [AdminController::class, 'edit'])->name('admin.edit');
 Route::patch('/admin', [AdminController::class, 'update'])->name('admin.update');
 Route::delete('/admin', [AdminController::class, 'destroy'])->name('admin.destroy');
+
+
 
 require __DIR__ . '/auth.php';
